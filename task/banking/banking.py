3bd62b6d4be4e7218183e8fd2e88a55ab983d232
Zhumakul-Artykbay <artykbay.zhumakulov@mail.ru>
2020-10-05 15:46:52 +0600

    Completed project version

--- task/banking/banking.py
+++ task/banking/banking.py
@@ -63,16 +63,69 @@ Your card PIN:
 
             while logged:
                 print("""
-                1. Balance
-                2. Log out
-                0. Exit
+1. Balance
+2. Add income
+3. Do transfer
+4. Close account
+5. Log out
+0. Exit
                 """)
 
                 log_case = int(input())
 
                 if log_case == 1:
-                    print(f"Balance: {balance}")
+                    print(f"Balance: {conn.execute('SELECT balance FROM card WHERE number = input_card_num')}")
                 elif log_case == 2:
+                    add_balance = int(input())
+                    conn.execute(f'UPDATE card SET balance = balance + {add_balance} WHERE number = {input_card_num}')
+                    conn.commit()
+                    print("Income was added!")
+                elif log_case == 3:
+                    is_transfering = True
+                    while is_transfering:
+                        print("""
+    Transfer
+    Enter card number:""")
+
+                        transfer_card_num = input()
+                        ch_luhn_algorithm = []
+                        ch_luhn_algorithm[:0] = transfer_card_num
+                        ch_luhn_algorithm = [int(i) for i in ch_luhn_algorithm]
+
+                        for i in range(0, len(ch_luhn_algorithm)):
+                            if i % 2 == 0:
+                                ch_luhn_algorithm[i] *= 2
+                                if ch_luhn_algorithm[i] > 9:
+                                    ch_luhn_algorithm[i] -= 9
+
+                        if sum(ch_luhn_algorithm) % 10 == 0:
+                            cur = conn.cursor()
+                            cur.execute(f'SELECT number FROM card WHERE number = {transfer_card_num}')
+                            if cur.fetchone():
+                                if transfer_card_num != input_card_num:
+                                    print("Enter how much money you want to transfer:")
+                                    transfer_balance = int(input())
+                                    if transfer_balance <= conn.execute(f"SELECT balance FROM card WHERE number = {input_card_num}").fetchone()[0]:
+                                        conn.execute(f"UPDATE card SET balance = balance - {transfer_balance} WHERE number = {input_card_num}")
+                                        conn.execute(
+                                            f"UPDATE card SET balance = balance + {transfer_balance} WHERE number = {transfer_card_num}")
+                                        conn.commit()
+                                        print("Success!")
+                                        is_transfering = False
+                                    else:
+                                        print("Not enough money!")
+                                        is_transfering = False
+                                else:
+                                    print("You can't transfer money to the same account!")
+                            else:
+                                print("Such a card does not exist.")
+                        else:
+                            print("Probably you made a mistake in the card number. Please try again!")
+                elif log_case == 4:
+                    conn.execute(f'DELETE FROM card WHERE number = {input_card_num}')
+                    conn.commit()
+                    print("The account has been closed!")
+                elif log_case == 5:
                     print("You have successfully logged out!")
                     logged = False
                 elif log_case == 0:
